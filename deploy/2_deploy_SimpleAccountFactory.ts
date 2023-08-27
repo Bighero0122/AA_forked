ad2c2f7215b150c4b0a76b2dedbd71f94db3a8a9
Jaden Williams <bighero0122@gmail.com>
2023-08-27 17:15:35 +0800

    sync

--- deploy/2_deploy_SimpleAccountFactory.ts
+++ deploy/2_deploy_SimpleAccountFactory.ts
@@ -6,13 +6,6 @@ const deploySimpleAccountFactory: DeployFunction = async function (hre: HardhatR
   const provider = ethers.provider
   const from = await provider.getSigner().getAddress()
   const network = await provider.getNetwork()
-  // only deploy on local test network.
-
-  const forceDeployFactory = process.argv.join(' ').match(/simple-account-factory/) != null
-
-  if (!forceDeployFactory && network.chainId !== 31337 && network.chainId !== 1337) {
-    return
-  }
 
   const entrypoint = await hre.deployments.get('EntryPoint')
   await hre.deployments.deploy(
